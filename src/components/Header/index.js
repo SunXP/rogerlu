ea87b168906502abc384cecfb37d659a2385dd67
unknown <roger_lu_dude@hotmail.com>
2020-05-24 20:40:55 -0400

    Add structure and style with header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <header className="header">
+        <nav className="headerMenu">
+            <a href="#">Home</a>
+            <a href="#">Posts</a>
+            <a href="#">About Me</a>
+        </nav>
+        <div className="headerMenu">
+            <a href="#">Github</a>
+            <a href="#">LinkedIn</a>
+        </div>
+    </header>
+   )
+
+ }
+
+export default Header
