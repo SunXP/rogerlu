e19e7095d2faf3c6bd3422544dfec7b54b9950d9
unknown <roger_lu_dude@hotmail.com>
2020-05-25 23:34:31 -0400

    Make my picture look pretty :)

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -11,9 +11,9 @@ const Welcome = (props) => {
   return(
     <div className="welcome-container">
         <div className="welcome-text">
-            <h3>Hi, my name is Roger Lu and I'm a recent grad from Concordia University in Software Engineering.</h3>
-            <p>This website isn't just a portfolio for me, but a way to post about all the new things I learn and experience...</p>
-            <p>And not just in tech.</p>
+            <h2>Hi, my name is Roger Lu and I like to write code.</h2>
+            <p>Even so, this website isn't just a portfolio, but a way for me to post about all the new things I learn and experience...</p>
+            <p>and not just in tech.</p>
         </div>
         <div className="photo-container">
             <img className="photo" src={photo} />
